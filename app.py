4d7f6723f41cfe51f736f47e7dcd2c8c889d7afd
Huy Hoang <huyhoang0496@gmail.com>
2020-02-05 17:54:55 +0100

    .

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import time
 import json
 
 import requests
+from flask import Flask, request, jsonify, render_template, redirect
+from concurrent.futures import ThreadPoolExecutor
 
 import headless_driver
 
-from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -69,6 +70,7 @@ def get_group_price(prices):
 
 
 #region ################ notifications ################
+@app.route('/periodic_checker')
 def periodic_checker():
   while True:
 
@@ -77,11 +79,18 @@ def periodic_checker():
 
     for name, group in group_prices.items():
       if group['price'] < group['threshold']:
-        send_notification(name, group['price'], group['threshold'])
-    
+        # send_notification(name, group['price'], group['threshold'])
+        pass
+  
+    time.sleep(0.2)
+
     settings = get_json('settings')
     if not settings['notifications']:
       break
+
+    print('periodic_checker running')
+
+  return render_template('settings.html', state=settings['notifications'])
 #endregion
 
 
@@ -95,7 +104,12 @@ def notify():
 def toggle_notifications():
   settings = get_json('settings')
   settings['notifications'] = not settings['notifications']
-  set_settings(settings)
+  set_settings('settings', settings)
+  print('Notifications:', settings['notifications'])
+
+  if settings['notifications']:
+    redirect('/periodic_checker', code=302)
+  
   return render_template('settings.html', state=settings['notifications'])
 #endregion
 
