28ad03e37c79b957a3acc406124c078f78c0c253
Huy Hoang <huyhoang0496@gmail.com>
2020-02-05 18:26:31 +0100

    .

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import json
 import requests
 from flask import Flask, request, jsonify, render_template, redirect
 from concurrent.futures import ThreadPoolExecutor
+from threading import Thread
+
 
 import headless_driver
 
@@ -30,7 +32,8 @@ def get_all_prices():
   links = products['links']
   thresholds = products['single_products']
   prices = {}
-  total = 0
+  total_price = 0
+  total_threshold = 0
   for name, link in links.items():
     price = get_price(link)
     product_detail = {}
@@ -38,9 +41,15 @@ def get_all_prices():
     product_detail['threshold'] = prettify_price(thresholds[name])
 
     prices[name] = product_detail
-    total += float(price[:-1])
+    total_price += float(price[:-1])
+    total_threshold += thresholds[name]
 
-  return prices, prettify_price(total)
+  total = {
+    'price': prettify_price(total_price),
+    'threshold': prettify_price(total_threshold)
+  }
+
+  return prices, total
 
 
 def get_price(link):
@@ -70,7 +79,7 @@ def get_group_price(prices):
 
 
 #region ################ notifications ################
-@app.route('/periodic_checker')
+# @app.route('/periodic_checker')
 def periodic_checker():
   while True:
 
@@ -79,10 +88,10 @@ def periodic_checker():
 
     for name, group in group_prices.items():
       if group['price'] < group['threshold']:
-        # send_notification(name, group['price'], group['threshold'])
+        send_notification(name, group['price'], group['threshold'])
         pass
   
-    time.sleep(0.2)
+    time.sleep(600)
 
     settings = get_json('settings')
     if not settings['notifications']:
@@ -108,7 +117,9 @@ def toggle_notifications():
   print('Notifications:', settings['notifications'])
 
   if settings['notifications']:
-    redirect('/periodic_checker', code=302)
+    thread = Thread(target=periodic_checker)
+    thread.daemon = True
+    thread.start()
   
   return render_template('settings.html', state=settings['notifications'])
 #endregion
